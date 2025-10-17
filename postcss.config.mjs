ed348cd0a4190aa3225991b3c8697779b409e51c
parasmottan <parasmottan9316@gmail.com>
2025-10-17 19:08:58 +0530

    commit

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,16 @@
+// postcss.config.js
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    // 1. Tailwind CSS ka plugin jo @tailwind directives ko process karta hai
+    'tailwindcss': {},
+    
+    // 2. Autoprefixer plugin, jo purane browsers (like kuch TV browsers)
+    // ke liye zaroori vendor prefixes (-webkit-, -moz- etc.) add karta hai.
+    'autoprefixer': {},
+  },
 };
 
 export default config;
+
+// Note: Agar tum .js file use kar rahe ho, toh "export default config"
+// ki jagah "module.exports = config;" use karna zyada common hai.
