b824b984a0d625d14e09177f4b28cdeeaa2d81b5
parasmottan <parasmottan9316@gmail.com>
2025-10-17 19:22:24 +0530

    added

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning> 
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
